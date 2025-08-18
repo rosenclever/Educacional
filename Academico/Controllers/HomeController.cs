9777706d5fd83d3c08f459d771a3ff0fdc453c7c
rosenclever.gazoni <rosenclever.gazoni@TPCP04LAB1100>
2025-08-18 19:28:44 -0300

    Adicionar arquivos de projeto.

---
+++ Academico/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Teste()
+        {
+            ViewData["Nome"] = "Seu nome";
+            return View();
+        }
+
+        public IActionResult Sobre() => View(); 
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Academico/Models/Aluno.cs
@@ -0,0 +1,9 @@
+﻿namespace Academico.Models
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public long Cep { get; set; }
+    }
+}
