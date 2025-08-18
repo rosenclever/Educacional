3c151bb1e86a9acfcb779c8c2a25d59da28a9ec3
rosenclever <rosenclever@gmail.com>
2025-08-18 19:54:56 -0300

    adicionada action create httppost

--- Academico/Controllers/AlunoController.cs
+++ Academico/Controllers/AlunoController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Academico.Controllers
 {
     public class AlunoController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Aluno aluno)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
